d5bb4bcd8bb5c659dcdb606824e021ba702536f3
ugo7953544 <ugo@okgo.fr>
2025-11-23 15:00:53 +0800

    delete: text-base className

--- components/ui/number-input.tsx
+++ components/ui/number-input.tsx
@@ -112,7 +112,7 @@ export function NumberInput({
         placeholder={placeholder}
         placeholderTextColor={placeholderTextColor}
         className={cn(
-          'web:flex h-10 native:h-12 web:w-full rounded-md border border-input bg-background px-3 web:py-2 text-base lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
+          'web:flex h-10 native:h-12 web:w-full rounded-md border border-input bg-background px-3 web:py-2 lg:text-sm native:text-lg native:leading-[1.25] text-foreground placeholder:text-muted-foreground web:ring-offset-background file:border-0 file:bg-transparent file:font-medium web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
           isFocused && 'web:ring-2 web:ring-ring web:ring-offset-2',
           className
         )}
