7a4f9b8d568750179e61b11b8163cf3e02ec8aea
ugo7953544 <ugo@okgo.fr>
2026-04-12 11:47:08 +0200

    add: opti css sidePanel

--- components/SidePanel.tsx
+++ components/SidePanel.tsx
@@ -220,7 +220,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: '600',
     color: '#2A2E33',
     maxWidth: 280,
     textAlign: 'left',
