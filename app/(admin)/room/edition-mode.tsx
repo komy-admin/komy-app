3cd888ff697a38327810766e228fc5aa272a9ce0
ugo7953544 <ugo@okgo.fr>
2026-04-18 14:39:24 +0200

    delete: shadow

--- app/(admin)/room/edition-mode.tsx
+++ app/(admin)/room/edition-mode.tsx
@@ -468,10 +468,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#EF4444',
     justifyContent: 'center',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 8,
-    elevation: 8,
   },
 });
